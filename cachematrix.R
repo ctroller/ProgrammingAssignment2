fbc166891b6cca7a4b25f5267831703e8b65841f
Christian Troller <ch_troller@bluemail.ch>
2015-01-13 22:26:34 +0100

    Fancier documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## The following functions provide utility to create and inverse a special matrix
-## that has the ability to cache its inversed matrix
+# The following functions provide utility to create and inverse a special matrix
+# that has the ability to cache its inversed matrix
+# @author Christian Troller <mailto:info@christiantroller.ch>
+# @version 1.0
+# @since 2015-01-13
 
-## Creates a special matrix object that can cache it's inverse
+# Creates a special matrix object that can cache it's inverse
+# @param x the matrix to make fancy
+# @return a list of attributes (set, get, setInverse, getInverse) for the given matrix
 makeCacheMatrix <- function(x = matrix()) {
     mInverse <- NULL
     
@@ -17,7 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Computes the inverse of the given special matrix `x` returned by makeCacheMatrix.
+# Computes the inverse of the given special matrix `x` returned by makeCacheMatrix.
+# @param x the special matrix
+# @return the inversed matrix of the given matrix
 cacheSolve <- function(x, ...) {
     m <- x$getInverse()
     if(!is.null(m)) {
